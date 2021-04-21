d9161345cf2b90540cad24d47f2427888c4df8f8
boughaba <khalid167995@gmail.com>
2021-04-21 23:17:36 +0000

    modified on background body

---
+++ Random Class For Body On Refresh/js/master.js
@@ -0,0 +1,3 @@
+var classlist = ["background-one", "background-two", "background-three", "background-four", "background-five"],
+    randomKey = Math.floor(Math.random() * classlist.length);
+    document.body.setAttribute('class', classlist[randomKey]);
