18903638edd261ab11cad31bda6e91ca976e10f7
KhalidBoughaba <boughaba.khalid.dev@gmail.com>
2021-04-26 10:17:35 +0000

    added file disable right click on page

--- Random Class For Body On Refresh/js/master.js
+++ Random Class For Body On Refresh/js/master.js
@@ -1,3 +1,5 @@
 var classlist = ["background-one", "background-two", "background-three", "background-four", "background-five"],
+
     randomKey = Math.floor(Math.random() * classlist.length);
+    
     document.body.setAttribute('class', classlist[randomKey]);
